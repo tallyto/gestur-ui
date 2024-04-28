b7fce207f3a8c7d801ef97ba4e176fe72913afef
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-27 22:02:18 -0300

    Fazer deploy do front-end no github-pages para validação do cliente

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,20 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cliente } from '../models/cliente.model';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  getClienteById(clienteId: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.baseUrl}/${clienteId}`);
-  }
-  getClientes(): Observable<Cliente[]> {
-    return this.http.get<Cliente[]>(this.baseUrl);
-  }
 
-  private baseUrl = 'http://localhost:3001/api/clientes'; 
+  private baseUrl = `${environment.apiUrl}/api/clientes`;
   constructor(private http: HttpClient) { }
 
   cadastrarCliente(cliente: Cliente): Observable<Cliente> {
@@ -26,4 +21,12 @@ export class ClienteService {
     return this.http.put<Cliente>(`${this.baseUrl}/${clienteId}`, cliente);
 
   }
+
+  getClienteById(clienteId: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.baseUrl}/${clienteId}`);
+  }
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.baseUrl);
+  }
+
 }
